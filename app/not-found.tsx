88c00a16dbc626dd1eccf2fa439bd0b2875515ad
Ohloule <louislayani2@gmail.com>
2026-01-23 15:33:59 +0100

    FIN2

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center bg-background text-foreground p-4">
-      <h1 className="text-8xl font-bold text-primary mb-4 animate-bounce font-special">
+      <h1 className="text-8xl font-bold text-principale-700 mb-4 animate-bounce font-special">
         404
       </h1>
       <h2 className="text-3xl md:text-4xl font-semibold mb-6">
