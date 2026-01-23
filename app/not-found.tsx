547c31f6b79a98821c35dd29f5547675a480c150
Ohloule <louislayani2@gmail.com>
2026-01-23 15:35:49 +0100

    my8

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center bg-background text-foreground p-4">
-      <h1 className="text-8xl font-bold text-principale-700 mb-4 animate-bounce font-special">
+      <h1 className="text-8xl font-bold text-principale-700 my-8 mb-4 animate-bounce font-special">
         404
       </h1>
       <h2 className="text-3xl md:text-4xl font-semibold mb-6">
@@ -34,7 +34,9 @@ export default function NotFound() {
           <Button className="w-48">Retour à l'accueil</Button>
         </Link>
         <Link href="/contact">
-          <Button className="w-48" variant="outline">Contacter le support</Button>
+          <Button className="w-48" variant="outline">
+            Contacter le support
+          </Button>
         </Link>
       </div>
     </div>
